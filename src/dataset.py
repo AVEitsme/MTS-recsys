6e4c68edb321c50d3a2193da60b3f437561e43e1
Artem Egorov <aveitsme@gmail.com>
2023-02-25 13:49:53 +0300

    [~] Auto split

--- src/dataset.py
+++ src/dataset.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from dataclasses import dataclass
+from src.config import Config
 
 
 class DatasetProcessor:
@@ -23,3 +25,17 @@ class DatasetProcessor:
             source[~source.index.isin(test_indexes)],
             source[source.index.isin(test_indexes)]
         ]
+
+
+@dataclass
+class Interactions:
+    source: pd.DataFrame
+    train: pd.DataFrame = None
+    test: pd.DataFrame = None
+    valid: pd.DataFrame = None
+
+    
+    def __init__(self, source: pd.DataFrame) -> None:
+        self.source = source
+        self.train, self.test = DatasetProcessor.split_train_test(source, "start_date", Config.TEST_DAYS)
+        self.train, self.valid = DatasetProcessor.split_train_test(self.train, "start_date", Config.TEST_DAYS * 2)
